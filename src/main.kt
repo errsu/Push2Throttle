5cbf60249b35b6a94e43f4de8407ea2739947600
errsu <errsuberlin@gmail.com>
2018-05-19 10:15:47 +0200

    JMRI: first contact
    3pp:
    
    nv-websocket-client-2.4.0.jar from
      https://github.com/TakahikoKawasaki/nv-websocket-client
      built from sources using IDEA
      version was b193e4e81e10f2f79f00fba11c95bb9f7dc21761
      from 17.04.2018  20:44:28
    tornadofx probably from https://github.com/edvin/tornadofx/releases
    usb4java see readme file

---
+++ src/main.kt
@@ -0,0 +1,93 @@
+import tornadofx.*
+import java.io.*
+import com.neovisionaries.ws.client.*
+
+class MyApp: App(MyView::class)
+
+class MyView: View() {
+
+    override val root = vbox {
+        button("TestRun") {
+            action {
+                EchoClient.main(arrayOf())
+            }
+        }
+    }
+}
+
+object EchoClient {
+    /**
+     * The echo server on websocket.org.
+     */
+    // private val SERVER = "ws://echo.websocket.org"
+    private val SERVER = "ws://localhost:12080/json/"
+
+    /**
+     * The timeout value in milliseconds for socket connection.
+     */
+    private val TIMEOUT = 5000
+
+
+    /**
+     * Wrap the standard input with BufferedReader.
+     */
+    private val input: BufferedReader
+        @Throws(IOException::class)
+        get() = BufferedReader(InputStreamReader(System.`in`))
+
+
+    /**
+     * The entry point of this command line application.
+     */
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // Connect to the echo server.
+        val ws = connect()
+
+        ws.sendText("""{"type":"ping"}""")
+        ws.sendText("""{"type":"throttle","data":{"throttle":"L0","address":0}}""")
+        ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":1.0}}""")
+        ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":0.8}}""")
+        ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":0.4}}""")
+        ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":0.2}}""")
+
+        // The standard input via BufferedReader.
+        val inStream = input
+
+        // A text read from the standard input.
+
+
+        // Read lines until "exit" is entered.
+        while (true) {
+            var text = inStream.readLine() ?: break
+            if (text == "exit") {
+                // Finish this application.
+                break
+            }
+            // Send the text to the server.
+            ws.sendText(text)
+        }
+        // Close the WebSocket.
+        ws.disconnect()
+    }
+
+
+    /**
+     * Connect to the server.
+     */
+    @Throws(IOException::class, WebSocketException::class)
+    private fun connect(): WebSocket {
+        return WebSocketFactory()
+                .setConnectionTimeout(TIMEOUT)
+                .createSocket(SERVER)
+                .addListener(object : WebSocketAdapter() {
+                    // A text message arrived from the server.
+                    override fun onTextMessage(websocket: WebSocket?, message: String?) {
+                        println(message)
+                    }
+                })
+                .addExtension(WebSocketExtension.PERMESSAGE_DEFLATE)
+                .connect()
+    }
+}

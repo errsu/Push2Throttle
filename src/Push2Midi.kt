5f30a3ed8e1ccc1c6a25b64bf977a72781d528ea
errsu <errsuberlin@gmail.com>
2018-05-19 12:38:16 +0200

    open Push2 ports and actually send a note

--- src/Push2Midi.kt
+++ src/Push2Midi.kt
@@ -1,48 +1,57 @@
 
-import javax.sound.midi.Synthesizer
 import javax.sound.midi.MidiUnavailableException
 import javax.sound.midi.MidiSystem
 import javax.sound.midi.MidiDevice
 import java.util.Vector
+import javax.sound.midi.ShortMessage
+
+
 
 class Push2Midi () {
     fun x() {
         println("looking for MIDI devices")
-        val synthInfos: Vector<*>
-        var device: MidiDevice
+        var push2InPort: MidiDevice? = null
+        var push2OutPort: MidiDevice? = null
         val infos = MidiSystem.getMidiDeviceInfo()
         for (i in infos.indices) {
             try {
-                device = MidiSystem.getMidiDevice(infos[i])
-                val device_ins = device.getMaxTransmitters()
-                val device_outs = device.getMaxReceivers()
-                val device_ins_text = if (device_ins == -1) "unlimited" else device_ins.toString()
-                val device_outs_ext = if (device_outs == -1) "unlimited" else device_outs.toString()
-
-                println("$i: ${device.deviceInfo.name} / ${device.deviceInfo.description}")
-                println("   ${device.maxReceivers} ${device.maxTransmitters} ${device.javaClass}")
-                println("   ${device.javaClass.name}")
-                println("   ins: ${device_ins_text} outs: ${device_outs_ext}")
-
-                if (device.deviceInfo.name == "Ableton Push 2")
-                {
-                    if (device.getMaxTransmitters() != 0)
-                    {
-                        println("---> PUSH2 MIDI input port - receive midi from here")
+                if (infos[i].name == "Ableton Push 2") {
+                    val push2 = MidiSystem.getMidiDevice(infos[i])
+                    if (push2.maxTransmitters != 0) {
+                        push2InPort = push2
+                        push2InPort.open()
                     }
-                    if (device.getMaxReceivers() != 0)
-                    {
-                        println("---> PUSH2 MIDI output port - send midi here")
+                    else if (push2.maxReceivers != 0) {
+                        push2OutPort = push2
+                        push2OutPort.open()
                     }
                 }
-
             } catch (e: MidiUnavailableException) {
                 println(e.message)
             }
-//            if (device is Synthesizer) {
-//                synthInfos.add(infos[i])
-//            }
         }
 
+        println("$push2OutPort open: ${push2OutPort?.isOpen}")
+        if (push2OutPort != null) {
+            println("push2InPort")
+            println("receivers: ${push2OutPort.receivers.size}")
+            println("transmitters: ${push2OutPort.transmitters.size}")
+            val rx = push2OutPort.getReceiver()
+            println("rx: ${rx}")
+            val myMsg = ShortMessage()
+            val white = 122
+            myMsg.setMessage(ShortMessage.NOTE_ON, 0, 36, white)
+            val timeStamp: Long = -1
+            rx.send(myMsg, timeStamp)
+        }
+
+        println("$push2InPort open: ${push2InPort?.isOpen}")
+        if (push2InPort != null) {
+            println("push2OutPort")
+            println("receivers: ${push2InPort.receivers.size}")
+            println("transmitters: ${push2InPort.transmitters.size}")
+            val tx = push2InPort.getTransmitter()
+            println("tx: ${tx}")
+        }
     }
 }

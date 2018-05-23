84d70c0090d3408b7cc7eafd3209e2dd51f2cd1d
errsu <errsuberlin@gmail.com>
2018-05-23 17:49:24 +0200

    recorded some responses to webserver messages
    - to start the webserver, goto "Handlungen->Starte Webserver"
    - WiThrottleServer is not needed

--- src/main.kt
+++ src/main.kt
@@ -52,12 +52,31 @@ object EchoClient {
         // Connect to the echo server.
         val ws = connect()
 
+        // RSUMSG: {"type":"hello","data":{"JMRI":"4.10+R419243e","json":"4.0",
+        //          "heartbeat":13500,"railroad":"Meine JMRI Bahngesellschaft",
+        //          "node":"jmri-309C233EFC0E-3df9f267","activeProfile":"ErrsuSimulationsAG"}}
         ws.sendText("""{"type":"ping"}""")
+        // RSUMSG: {"type":"pong"}
         ws.sendText("""{"type":"throttle","data":{"throttle":"L0","address":0}}""")
+        // RSUMSG: {"type":"throttle","data":{"address":0,"speed":0.0,"forward":true,
+        //          "F0":false,"F1":false,"F2":false,"F3":false,"F4":false,"F5":false,
+        //          "F6":false,"F7":false,"F8":false,"F9":false,"F10":false,"F11":false,
+        //          "F12":false,"F13":false,"F14":false,"F15":false,"F16":false,"F17":false,
+        //          "F18":false,"F19":false,"F20":false,"F21":false,"F22":false,"F23":false,
+        //          "F24":false,"F25":false,"F26":false,"F27":false,"F28":false,
+        //          "speedSteps":126,"clients":1,"throttle":"L0"}}
         ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":1.0}}""")
+        // RSUMSG: {"type":"throttle","data":{"speed":1.0,"throttle":"L0"}}
         ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":0.8}}""")
+        // RSUMSG: {"type":"throttle","data":{"speed":0.8,"throttle":"L0"}}
         ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":0.4}}""")
+        // RSUMSG: {"type":"throttle","data":{"speed":0.4,"throttle":"L0"}}
         ws.sendText("""{"type":"throttle","data":{"throttle":"L0","speed":0.2}}""")
+        // RSUMSG: {"type":"throttle","data":{"speed":0.2,"throttle":"L0"}}
+
+        // how to listen to incoming changes:
+        // {"type":"throttle","data":{"throttle":"L0"}}
+        // RSUMSG: {"type":"throttle","data":{"speed":0.75396824,"throttle":"L0"}}
 
         // The standard input via BufferedReader.
         val inStream = input
@@ -72,8 +91,13 @@ object EchoClient {
                 // Finish this application.
                 break
             }
-            // Send the text to the server.
-            ws.sendText(text)
+            if (text == "ping") {
+                ws.sendPing()
+            }
+            else {
+                // Send the text to the server.
+                ws.sendText(text)
+            }
         }
         // Close the WebSocket.
         ws.disconnect()
@@ -91,7 +115,17 @@ object EchoClient {
                 .addListener(object : WebSocketAdapter() {
                     // A text message arrived from the server.
                     override fun onTextMessage(websocket: WebSocket?, message: String?) {
-                        println(message)
+                        println("RSUMSG: " + message)
+                    }
+
+                    override fun onPingFrame(websocket: WebSocket?, frame: WebSocketFrame?) {
+                        super.onPingFrame(websocket, frame)
+                        println("ERRSUPING: ping received")
+                    }
+
+                    override fun onPongFrame(websocket: WebSocket?, frame: WebSocketFrame?) {
+                        super.onPongFrame(websocket, frame)
+                        println("ERRSUPONG: pong received")
                     }
                 })
                 .addExtension(WebSocketExtension.PERMESSAGE_DEFLATE)

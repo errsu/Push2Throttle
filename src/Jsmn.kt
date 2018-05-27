9110583daf350968ce94ef2e6475189ee8470373
errsu <errsuberlin@gmail.com>
2018-05-27 14:50:09 +0200

    extract data from parser as tree of objects
    tokens are represented as follows
    - STRING -> String
    - PRIMITIVE -> Boolean, Int, Float or null
    - OBJECT -> Map
    - ARRAY -> List

--- src/Jsmn.kt
+++ src/Jsmn.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 enum class JsmnType {
     UNDEFINED,
     OBJECT,
@@ -12,17 +14,17 @@ const val JSMN_ERROR_NOMEM = -1 // Not enough tokens were provided
 const val JSMN_ERROR_INVAL = -2 // Invalid character inside JSON string
 const val JSMN_ERROR_PART  = -3 // The string is not a full JSON packet, more bytes expected
 
-class JsmnParser(private val num_tokens: Int) {
+class JsmnParser(private val numTokens: Int) {
     // JSON parser. Contains an array of token blocks available. Also stores
     // the string being parsed now and current position in that string
-    private var pos = 0   // offset in the JSON string
-    private var toknext = 0   // next token to allocate
+    private var pos = 0        // offset in the JSON string
+    private var toknext = 0    // next token to allocate
     private var toksuper = -1  // superior token node, e.g parent object or array
 
-    val tokens = Array(num_tokens) { JsmnTok(JsmnType.UNDEFINED, -1, -1, -1) }
+    val tokens = Array(numTokens) { JsmnTok(JsmnType.UNDEFINED, -1, -1, -1) }
 
 	private fun allocToken() : JsmnTok? {
-		if (toknext >= num_tokens) {
+		if (toknext >= numTokens) {
 			return null
 		}
 		val token = tokens[toknext++]
@@ -236,30 +238,133 @@ class JsmnParser(private val num_tokens: Int) {
 
         return count
     }
+
+    private fun tokenToPrimitive(js: String, tok: Int) : Pair<Any?, Int> {
+        val primitiveToken = tokens[tok]
+        if (primitiveToken.type == JsmnType.PRIMITIVE)
+        {
+            val primitive = js.substring(primitiveToken.start, primitiveToken.end)
+            return Pair(when {
+                            primitive == "true"  -> true
+                            primitive == "false" -> false
+                            primitive == "null"  -> null
+                            '.' in primitive     -> primitive.toFloat()
+                            else                 -> primitive.toInt()
+                        }, tok + 1)
+        } else {
+            throw Error("primitive expected at ${primitiveToken.start}")
+        }
+    }
+
+    private fun tokenToString(js : String, tok: Int) : Pair<String, Int> {
+        val stringToken = tokens[tok]
+        if (stringToken.type == JsmnType.STRING) {
+            return Pair(js.substring(stringToken.start, stringToken.end), tok + 1)
+        } else {
+            throw Error("string expected at ${stringToken.start}")
+        }
+    }
+
+    private fun tokenToMap(js : String, tok: Int) : Pair<Map<String, Any?>, Int> {
+        val objectToken = tokens[tok]
+        if (objectToken.type == JsmnType.OBJECT) {
+            val result : MutableMap<String, Any?> = HashMap()
+            var nextTok = tok + 1
+            repeat(objectToken.size) {
+                val keyToken = tokens[nextTok]
+                val (key, tokAfterKey) = tokenToString(js, nextTok)
+                nextTok = tokAfterKey
+                if (keyToken.size == 1) {
+                    val (value, tokAfterValue) = tokenToAny(js, nextTok)
+                    nextTok = tokAfterValue
+                    result[key] = value
+                } else {
+                    result[key] = null
+                }
+            }
+            return Pair(result, nextTok)
+        } else {
+            throw Error("object expected at ${objectToken.start}")
+        }
+    }
+
+    private fun tokenToList(js : String, tok: Int) : Pair<List<Any?>, Int> {
+        val arrayToken = tokens[tok]
+        if (arrayToken.type == JsmnType.ARRAY) {
+            val result : Array<Any?> = Array(arrayToken.size) { null }
+            var nextTok = tok + 1
+            repeat(arrayToken.size) {
+                val (value, tokAfterValue) = tokenToAny(js, nextTok)
+                nextTok = tokAfterValue
+                result[it] = value
+            }
+            return Pair(result.toList(), nextTok)
+        } else {
+            throw Error("array expected at ${arrayToken.start}")
+        }
+    }
+
+    private fun tokenToAny(js : String, tok: Int) : Pair<Any?, Int> {
+        val token = tokens[tok]
+        return when (token.type) {
+            JsmnType.STRING    -> tokenToString(js, tok)
+            JsmnType.PRIMITIVE -> tokenToPrimitive(js, tok)
+            JsmnType.OBJECT    -> tokenToMap(js, tok)
+            JsmnType.ARRAY     -> tokenToList(js, tok)
+            else -> throw Error("unexpected token type")
+        }
+    }
+
+    fun parseToTree(js: String) : Any? {
+        if (tokens.isEmpty()) return null
+        val tokCount = parse(js)
+        if (tokCount <= 0) return null
+        return try {
+            val (result, tokAfterAny) = tokenToAny(js, 0)
+            if (tokAfterAny != tokCount) {
+                throw Error("not all tokens used")
+            } else {
+                result
+            }
+        } catch (e: Error) {
+            println("Jsmn::parseToTree Error: ${e.message}")
+            null
+        }
+    }
 }
 
 fun testJsmn() {
-    val parserZ = JsmnParser(0)
-    testCase(parserZ, """{"type":"throttle","data":{"throttle":"L0","speed":-1.0}}""")
-    testCase(parserZ, """{"a":"b","c":[ 1, 2, 3 ],"d": null}""")
-    testCase(parserZ, """{"a"}""")
-    testCase(parserZ, """{"a", "b", "c" : false}""")
-
-    val parser = JsmnParser(10)
-    testCase(parser, """{"type":"throttle","data":{"throttle":"L0","speed":-1.0}}""")
-    testCase(parser, """{"a":"b","c":[ 1, 2, 3 ],"d": null}""")
-    testCase(parser, """{"a"}""")
-    testCase(parser, """{"a", "b", "c" : false}""")
+    testCase("""{"type":"throttle","data":{"throttle":"L0","speed":-1.0}}""")
+    testCase("""{"a":"b","c":[ 1, 2, 3 ],"d": null}""")
+    testCase("""{"a"}""")
+    testCase("""{"a", "b", "c" : false}""")
+    testCase("""[1, 2, 3]""")
+    testCase("""{1, [1, 2]}""")
+    testCase("""{1 : "bad key"}""")
+    testCase("""{[] : "another bad key"}""")
+    testCase("""{{} : "another bad key"}""")
+    testCase("""{true: "final bad key"}""")
+    testCase("""{"bad value" : 2 + 3}""")
 }
 
-fun testCase(parser: JsmnParser, js: String) {
+fun testCase(js: String) {
     println(js)
-    val n = parser.parse(js)
-    println("tokens returned: $n")
-    if (parser.tokens.isNotEmpty()) {
-        for (i in 0 until n) {
-            val token = parser.tokens[i]
-            println("${token} '${js.substring(token.start, token.end)}'")
+    val parserZ = JsmnParser(0)
+    val n = parserZ.parse(js)
+    println("tokens detected: $n")
+    if (n > 0) {
+        val parser = JsmnParser(n)
+        val n1 = parser.parse(js)
+        if (n1 != n) {
+            println("got $n1 tokens when reparsing")
+        }
+        if (n1 > 0) {
+            for (i in 0 until n1) {
+                val token = parser.tokens[i]
+                println("[$i] ${token} '${js.substring(token.start, token.end)}'")
+            }
+            val tree = parser.parseToTree(js)
+            println("Tree: $tree")
         }
     }
     println("-----------------------------------------------------------------------------")
@@ -269,53 +374,84 @@ fun testCase(parser: JsmnParser, js: String) {
 Test results:
 
 {"type":"throttle","data":{"throttle":"L0","speed":-1.0}}
-tokens returned: 9
+tokens detected: 9
+[0] JsmnTok(type=OBJECT, start=0, end=57, size=2) '{"type":"throttle","data":{"throttle":"L0","speed":-1.0}}'
+[1] JsmnTok(type=STRING, start=2, end=6, size=1) 'type'
+[2] JsmnTok(type=STRING, start=9, end=17, size=0) 'throttle'
+[3] JsmnTok(type=STRING, start=20, end=24, size=1) 'data'
+[4] JsmnTok(type=OBJECT, start=26, end=56, size=2) '{"throttle":"L0","speed":-1.0}'
+[5] JsmnTok(type=STRING, start=28, end=36, size=1) 'throttle'
+[6] JsmnTok(type=STRING, start=39, end=41, size=0) 'L0'
+[7] JsmnTok(type=STRING, start=44, end=49, size=1) 'speed'
+[8] JsmnTok(type=PRIMITIVE, start=51, end=55, size=0) '-1.0'
+Tree: {data={throttle=L0, speed=-1.0}, type=throttle}
 -----------------------------------------------------------------------------
 {"a":"b","c":[ 1, 2, 3 ],"d": null}
-tokens returned: 10
+tokens detected: 10
+[0] JsmnTok(type=OBJECT, start=0, end=35, size=3) '{"a":"b","c":[ 1, 2, 3 ],"d": null}'
+[1] JsmnTok(type=STRING, start=2, end=3, size=1) 'a'
+[2] JsmnTok(type=STRING, start=6, end=7, size=0) 'b'
+[3] JsmnTok(type=STRING, start=10, end=11, size=1) 'c'
+[4] JsmnTok(type=ARRAY, start=13, end=24, size=3) '[ 1, 2, 3 ]'
+[5] JsmnTok(type=PRIMITIVE, start=15, end=16, size=0) '1'
+[6] JsmnTok(type=PRIMITIVE, start=18, end=19, size=0) '2'
+[7] JsmnTok(type=PRIMITIVE, start=21, end=22, size=0) '3'
+[8] JsmnTok(type=STRING, start=26, end=27, size=1) 'd'
+[9] JsmnTok(type=PRIMITIVE, start=30, end=34, size=0) 'null'
+Tree: {a=b, c=[1, 2, 3], d=null}
 -----------------------------------------------------------------------------
 {"a"}
-tokens returned: 2
+tokens detected: 2
+[0] JsmnTok(type=OBJECT, start=0, end=5, size=1) '{"a"}'
+[1] JsmnTok(type=STRING, start=2, end=3, size=0) 'a'
+Tree: {a=null}
 -----------------------------------------------------------------------------
 {"a", "b", "c" : false}
-tokens returned: 5
+tokens detected: 5
+[0] JsmnTok(type=OBJECT, start=0, end=23, size=3) '{"a", "b", "c" : false}'
+[1] JsmnTok(type=STRING, start=2, end=3, size=0) 'a'
+[2] JsmnTok(type=STRING, start=7, end=8, size=0) 'b'
+[3] JsmnTok(type=STRING, start=12, end=13, size=1) 'c'
+[4] JsmnTok(type=PRIMITIVE, start=17, end=22, size=0) 'false'
+Tree: {a=null, b=null, c=false}
 -----------------------------------------------------------------------------
-{"type":"throttle","data":{"throttle":"L0","speed":-1.0}}
-tokens returned: 9
-JsmnTok(type=OBJECT, start=0, end=57, size=2) '{"type":"throttle","data":{"throttle":"L0","speed":-1.0}}'
-JsmnTok(type=STRING, start=2, end=6, size=1) 'type'
-JsmnTok(type=STRING, start=9, end=17, size=0) 'throttle'
-JsmnTok(type=STRING, start=20, end=24, size=1) 'data'
-JsmnTok(type=OBJECT, start=26, end=56, size=2) '{"throttle":"L0","speed":-1.0}'
-JsmnTok(type=STRING, start=28, end=36, size=1) 'throttle'
-JsmnTok(type=STRING, start=39, end=41, size=0) 'L0'
-JsmnTok(type=STRING, start=44, end=49, size=1) 'speed'
-JsmnTok(type=PRIMITIVE, start=51, end=55, size=0) '-1.0'
+[1, 2, 3]
+tokens detected: 4
+[0] JsmnTok(type=ARRAY, start=0, end=9, size=3) '[1, 2, 3]'
+[1] JsmnTok(type=PRIMITIVE, start=1, end=2, size=0) '1'
+[2] JsmnTok(type=PRIMITIVE, start=4, end=5, size=0) '2'
+[3] JsmnTok(type=PRIMITIVE, start=7, end=8, size=0) '3'
+Tree: [1, 2, 3]
 -----------------------------------------------------------------------------
-{"a":"b","c":[ 1, 2, 3 ],"d": null}
-tokens returned: 10
-JsmnTok(type=OBJECT, start=0, end=35, size=3) '{"a":"b","c":[ 1, 2, 3 ],"d": null}'
-JsmnTok(type=STRING, start=2, end=3, size=1) 'a'
-JsmnTok(type=STRING, start=6, end=7, size=0) 'b'
-JsmnTok(type=STRING, start=10, end=11, size=1) 'c'
-JsmnTok(type=ARRAY, start=13, end=24, size=3) '[ 1, 2, 3 ]'
-JsmnTok(type=PRIMITIVE, start=15, end=16, size=0) '1'
-JsmnTok(type=PRIMITIVE, start=18, end=19, size=0) '2'
-JsmnTok(type=PRIMITIVE, start=21, end=22, size=0) '3'
-JsmnTok(type=STRING, start=26, end=27, size=1) 'd'
-JsmnTok(type=PRIMITIVE, start=30, end=34, size=0) 'null'
+{1, [1, 2]}
+tokens detected: 5
+got -2 tokens when reparsing
 -----------------------------------------------------------------------------
-{"a"}
-tokens returned: 2
-JsmnTok(type=OBJECT, start=0, end=5, size=1) '{"a"}'
-JsmnTok(type=STRING, start=2, end=3, size=0) 'a'
+{1 : "bad key"}
+tokens detected: 3
+got -2 tokens when reparsing
 -----------------------------------------------------------------------------
-{"a", "b", "c" : false}
-tokens returned: 5
-JsmnTok(type=OBJECT, start=0, end=23, size=3) '{"a", "b", "c" : false}'
-JsmnTok(type=STRING, start=2, end=3, size=0) 'a'
-JsmnTok(type=STRING, start=7, end=8, size=0) 'b'
-JsmnTok(type=STRING, start=12, end=13, size=1) 'c'
-JsmnTok(type=PRIMITIVE, start=17, end=22, size=0) 'false'
+{[] : "another bad key"}
+tokens detected: 3
+[0] JsmnTok(type=OBJECT, start=0, end=24, size=1) '{[] : "another bad key"}'
+[1] JsmnTok(type=ARRAY, start=1, end=3, size=1) '[]'
+[2] JsmnTok(type=STRING, start=7, end=22, size=0) 'another bad key'
+Jsmn::parseToTree Error: string expected at 1
+Tree: null
+-----------------------------------------------------------------------------
+{{} : "another bad key"}
+tokens detected: 3
+[0] JsmnTok(type=OBJECT, start=0, end=24, size=1) '{{} : "another bad key"}'
+[1] JsmnTok(type=OBJECT, start=1, end=3, size=1) '{}'
+[2] JsmnTok(type=STRING, start=7, end=22, size=0) 'another bad key'
+Jsmn::parseToTree Error: string expected at 1
+Tree: null
+-----------------------------------------------------------------------------
+{true: "final bad key"}
+tokens detected: 3
+got -2 tokens when reparsing
+-----------------------------------------------------------------------------
+{"bad value" : 2 + 3}
+tokens detected: -2
 -----------------------------------------------------------------------------
 */

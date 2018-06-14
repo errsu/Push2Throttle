e9cd5093d1433ea08fecfaa4710bab12161611fb
errsu <errsuberlin@gmail.com>
2018-06-14 08:03:04 +0200

    draw some rectangles with correct colors
    - let the image be the (efficient) default color type
    - convert before sending
    - custom colors not needed
    - advantage is the IDEA shows color rectangle
      to the left of lines containing Color(<r>, <g>, <b>)

--- src/Push2DisplayDrawing.kt
+++ src/Push2DisplayDrawing.kt
@@ -1,5 +1,7 @@
+import java.awt.Color
 import java.awt.Font
-import java.awt.Graphics2D
+import java.awt.geom.Line2D
+import java.awt.geom.Rectangle2D
 import java.awt.image.BufferedImage
 import java.nio.ByteBuffer
 
@@ -9,30 +11,52 @@ class Push2DisplayDrawing() {
     private val displayHeight      = 160
     private val lineFiller         = 64 // extra unused pixels per line
 
-    private fun rgb(r: Int, g: Int, b: Int) : Short {
-        return (r).or(g.shl(5)).or(b.shl(11)).toShort()
-    }
-
-    private val white        = rgb(31, 63, 31)
-    private val black        = rgb(0, 0, 0)
-    private fun red(r: Int)  = rgb(r, 0, 0)
-    private fun grn(g: Int)  = rgb(0, g, 0)
-    private fun blu(b: Int)  = rgb(0, 0, b)
-    private fun gry(gr: Int) = rgb(gr, 2*gr, gr)
-
-    private val image = BufferedImage(displayWidth, displayHeight, BufferedImage.TYPE_USHORT_565_RGB)
+    private val image = BufferedImage(displayWidth, displayHeight, BufferedImage.TYPE_INT_ARGB)
     private val g = image.createGraphics()
     private val font24 = Font("SansSerif", Font.PLAIN, 24)
 
     private var lastFrame = -1
 
-    fun prepareFrame(frame: Int) {
+    private fun prepareFrame(frame: Int) {
         if (frame != lastFrame) {
+            val x0 = 0.0
+            val x1 = (displayWidth - 1).toDouble()
+            val y0 = 0.0
+            val y1 = (displayHeight - 1).toDouble()
+            val tw = (displayWidth / 8).toDouble() // track width
             g.clearRect(0, 0, displayWidth, displayHeight)
+            g.paint = Color(32, 64, 64)
+            g.fill(Rectangle2D.Double(x0, y0, x1, y1))
+            g.paint = Color(128, 255, 255)
+            var t0 = 0.0
+            for (n in 0..7) {
+                g.draw(Line2D.Double(t0, y0, t0, y1))
+                g.draw(Line2D.Double(t0 + tw - 1.0, y0, t0 + tw - 1.0, y1))
+                t0 += tw
+            }
+            g.draw(Line2D.Double(x0, y0, x1, y0))
+            g.draw(Line2D.Double(x0, y1, x1, y1))
             g.font = font24
+            g.paint = Color(255, 0, 0)
             g.drawString("Hello!", (frame % (displayWidth + 100)) - 50, 92)
             lastFrame = frame
         }
+    }
+
+    private fun Int.aRgbTo565bgr() : Short {
+
+        // a a a a a a a a r r r r r r r r g g g g g g g g b b b b b b b b
+        //                 + + + + +       * * * * * *     # # # # #
+        //        |       |       |       |       |       |       |
+        //                                 # # # # # * * * * * * + + + + +
+        // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 b b b b b g g g g g g r r r r r
+
+        val r = this.shr(19).and(0x0000001F)
+        val g = this.shr(5).and(0x000007E0)
+        val b = this.shl(8).and(0x0000F800)
+        val color = r.or(g).or(b)
+        // println("this: ${java.lang.Integer.toHexString(this)} color: ${java.lang.Integer.toHexString(color)}")
+        return color.toShort()
     }
 
     fun apply(buffer: ByteBuffer, frame: Int, firstLine: Int, lineCount: Int)
@@ -45,7 +69,7 @@ class Push2DisplayDrawing() {
         repeat(lineCount) {
             val line = firstLine + it
             repeat(displayWidth) {
-                shortBuffer.put(bufferIndex++, image.getRGB(it, line).toShort())
+                shortBuffer.put(bufferIndex++, image.getRGB(it, line).aRgbTo565bgr())
             }
             bufferIndex +=  lineFiller
         }

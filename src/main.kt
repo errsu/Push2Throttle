3956b5904609cbacb1e564aa2605d6f6a96198d1
errsu <errsuberlin@gmail.com>
2018-06-16 21:12:27 +0200

    show how to get power, roster, tournout and panel info

--- src/main.kt
+++ src/main.kt
@@ -68,6 +68,53 @@ class Push2ThrottleMainView: View() {
                 libUsbHelper.listDevices()
             }
         }
+        button("JSMN get more data") {
+            action {
+                getMoreData()
+            }
+        }
     }
 }
 
+val jmri: JmriWsClient = JmriWsClient()
+
+fun getMoreData() {
+    // to see the turnouts here, open panel pro from
+    // Decoder Pro -> File -> Open Panel Pro Window
+    // Then load the panels with Panel Pro -> Panels -> OpenPanels
+    // This can be automated:
+    // Decoder Pro -> Edit -> Preferences
+    // Aufstartverhalten -> Add (pull down) -> Open File
+    // and choose the panels/tournouts xml file
+    if (!jmri.isConnected()) jmri.connect(::jmriCallback)
+    jmri.sendTextMessage("""{"type":"power","data":{}}""")
+    jmri.sendTextMessage("""{"list":"roster"}""")
+    jmri.sendTextMessage("""{"list":"panels"}""")
+    jmri.sendTextMessage("""{"type":"turnout","data":{"name":"NT1","state":4}}""")
+    jmri.sendTextMessage("""{"type":"turnout","data":{"name":"NT22"}}""")
+    jmri.sendTextMessage("""{"list":"turnouts"}""")
+    // returns things like
+    // {name=IT1, comment=null, state=0, userName=WI0, inverted=false}
+    // {name=NT1, comment=null, state=4, userName=W0, inverted=false}
+}
+private fun jmriCallback(tree: Any?) {
+    println("$tree")
+    if (tree is Map<*,*> && tree["type"] != "pong") println("> $tree")
+    if (tree is List<*>) {
+        for (it in tree) {
+            println("---------------")
+            if (it is Map<*,*>) {
+                if (it["type"] == "rosterEntry") {
+                    val data = it["data"] as Map<*,*>
+                    println("name ${data["name"]}")
+                    println("address ${data["address"]}")
+                    println("maxSpeedPcct ${data["maxSpeedPct"]}")
+                }
+                if (it["type"] == "turnout") {
+                    val data = it["data"] as Map<*,*>
+                    println("$data")
+                }
+            }
+        }
+    }
+}

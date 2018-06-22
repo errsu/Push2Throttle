1c1dbd97968fafae1756a3a88e2ca652b9ce9608
errsu <errsuberlin@gmail.com>
2018-06-22 02:03:35 +0200

    show panel background

--- src/PanelDisplay.kt
+++ src/PanelDisplay.kt
@@ -1,12 +1,18 @@
 import java.awt.Graphics2D
+import java.io.File
+import javax.imageio.ImageIO
 
 class PanelDisplay {
 
     private val displayWidth  = 960
     private val displayHeight = 160
 
+    private val img = ImageIO.read(File("Panel.png"))
+
+    @Suppress("UNUSED_PARAMETER")
     fun drawFrame(g: Graphics2D, frame: Int, displayContent: Push2DisplayContent) {
-        g.paint = displayContent.push2Colors[(frame / 60) % 27]
-        g.fillRect(0, 0, displayWidth, displayHeight)
+//        g.paint = displayContent.push2Colors[(frame / 60) % 27]
+//        g.fillRect(0, 0, displayWidth, displayHeight)
+        g.drawImage(img, 0, 0, null)
     }
 }

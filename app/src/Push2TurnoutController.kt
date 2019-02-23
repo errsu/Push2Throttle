e333dfe4edc2f1455dcbe42cc00cf32db8c829d2
errsu <errsuberlin@gmail.com>
2019-02-23 18:18:03 +0100

    simplified turnout group handling by unification

--- app/src/Push2TurnoutController.kt
+++ app/src/Push2TurnoutController.kt
@@ -26,9 +26,6 @@ class Push2TurnoutController(
         }
     }
 
-    // TODO: update for 3way switches
-    // Have an element with three states!
-
     private fun turnoutGroupContainsTurnout(turnoutGroup: TurnoutGroup, turnout: Turnout) : Boolean {
         return when(turnoutGroup) {
             is TurnoutGroup.SingleTurnout ->
@@ -62,8 +59,8 @@ class Push2TurnoutController(
                 when (position) {
                     "straight" -> "green"
                     "cross"    -> "yellow"
-                    "turnWest" -> "blue"
-                    "turnEast" -> "red"
+                    "turnWest" -> "red"
+                    "turnEast" -> "blue"
                     "invalid"  -> "orange"
                     else -> "black"
                 }
@@ -76,14 +73,14 @@ class Push2TurnoutController(
                 when (position) {
                     "closed" -> "thrown"
                     "thrown" -> "closed"
-                    else -> "invalid"
+                    else -> "closed"
                 }
             is TurnoutGroup.ThreeWayTurnout ->
                 when (position) {
                     "left" -> "right"
                     "mid" -> "left"
                     "right" -> "mid"
-                    else -> "invalid"
+                    else -> "mid"
                 }
             is TurnoutGroup.DoubleSlipTurnout ->
                 when (position) {
@@ -91,7 +88,7 @@ class Push2TurnoutController(
                     "cross" -> "turnWest"
                     "turnWest" -> "turnEast"
                     "turnEast" -> "straight"
-                    else -> "invalid"
+                    else -> "straight"
                 }
         }
     }
@@ -128,28 +125,37 @@ class Push2TurnoutController(
         }
     }
 
+    private fun setTurnoutStates(turnoutGroup: TurnoutGroup, position: String, setState: (Turnout, Int) -> Unit) {
+        when (turnoutGroup) {
+            is TurnoutGroup.SingleTurnout ->
+                setState(turnoutGroup.turnout,
+                   when(position) {"thrown" -> TurnoutState.THROWN else -> TurnoutState.CLOSED })
+            is TurnoutGroup.ThreeWayTurnout -> {
+                setState(turnoutGroup.leftTurnout,
+                   when (position) {"left" -> TurnoutState.THROWN else -> TurnoutState.CLOSED })
+                setState(turnoutGroup.rightTurnout,
+                   when (position) {"right" -> TurnoutState.THROWN else -> TurnoutState.CLOSED })
+            }
+            is TurnoutGroup.DoubleSlipTurnout -> {
+                setState(turnoutGroup.westTurnout,
+                   when(position) {"cross", "turnWest" -> TurnoutState.THROWN else -> TurnoutState.CLOSED })
+                setState(turnoutGroup.eastTurnout,
+                   when(position) {"cross", "turnEast" -> TurnoutState.THROWN else -> TurnoutState.CLOSED })
+            }
+        }
+    }
+
     fun connectToElements() {
         turnoutGroups.forEachIndexed { index, turnoutGroup ->
             if (turnoutGroup != null) {
-                when(turnoutGroup) {
-                    is TurnoutGroup.SingleTurnout ->
-                        elements.connect(buttons[index], this,
-                                mapOf("onColor" to turnoutGroupColor(turnoutGroup, "thrown"),
-                                      "offColor" to turnoutGroupColor(turnoutGroup, "closed"),
-                                      "type" to "toggle"),
-                                turnoutGroup.turnout.state.value == TurnoutState.THROWN)
-                    is TurnoutGroup.ThreeWayTurnout,
-                    is TurnoutGroup.DoubleSlipTurnout -> {
-                        val position = turnoutGroupPosition(turnoutGroup)
-                        val nextPosition = getNextPosition(turnoutGroup, position)
-                        elements.connect(buttons[index], this,
-                                mapOf("onColor" to  turnoutGroupColor(turnoutGroup, nextPosition),
-                                      "offColor" to turnoutGroupColor(turnoutGroup, position),
-                                      "type" to "momentary"),
-                                      false
-                                )
-                    }
-                }
+                val position = turnoutGroupPosition(turnoutGroup)
+                val nextPosition = getNextPosition(turnoutGroup, position)
+                elements.connect(buttons[index], this,
+                        mapOf("onColor" to  turnoutGroupColor(turnoutGroup, nextPosition),
+                              "offColor" to turnoutGroupColor(turnoutGroup, position),
+                              "type" to "momentary"),
+                              false
+                        )
             }
         }
     }
@@ -164,24 +170,13 @@ class Push2TurnoutController(
         if (attrName == "state") {
             turnoutGroups.forEachIndexed { index, turnoutGroup ->
                 if (turnoutGroup != null) {
-                    when(turnoutGroup) {
-                        is TurnoutGroup.SingleTurnout ->
-                            if (turnoutGroupContainsTurnout(turnoutGroup, turnout)) {
-                                elements.updateElementStateByJmri(buttons[index], newValue == TurnoutState.THROWN)
-                                return
-                            }
-                        is TurnoutGroup.ThreeWayTurnout,
-                        is TurnoutGroup.DoubleSlipTurnout -> {
-                            if (turnoutGroupContainsTurnout(turnoutGroup, turnout)) {
-                                val position = turnoutGroupPosition(turnoutGroup)
-                                val nextPosition = getNextPosition(turnoutGroup, position)
-                                buttons[index].setAttributes(
-                                        mapOf("offColor" to turnoutGroupColor(turnoutGroup, position),
-                                              "onColor" to turnoutGroupColor(turnoutGroup, nextPosition),
-                                              "type" to "momentary"))
-                                return
-                            }
-                        }
+                    if (turnoutGroupContainsTurnout(turnoutGroup, turnout)) {
+                        val position = turnoutGroupPosition(turnoutGroup)
+                        val nextPosition = getNextPosition(turnoutGroup, position) // is this correct? what about newValue??
+                        buttons[index].setAttributes(
+                                mapOf("offColor" to turnoutGroupColor(turnoutGroup, position),
+                                      "onColor" to turnoutGroupColor(turnoutGroup, nextPosition)))
+                        return
                     }
                 }
             }
@@ -193,58 +188,11 @@ class Push2TurnoutController(
             for (index in buttons.indices) {
                 if (element == buttons[index]) {
                     val tog = turnoutGroups[index]
-                    if (tog != null) {
-                        when(tog) {
-                            is TurnoutGroup.SingleTurnout -> {
-                                turnoutTable.messageToJmri(
-                                    tog.turnout, "state",
-                                    if (newValue == true) TurnoutState.THROWN else TurnoutState.CLOSED
-                                )
-                            }
-                            is TurnoutGroup.ThreeWayTurnout -> {
-                                if (newValue == true) {
-                                    val position = turnoutGroupPosition(tog)
-                                    val nextPosition = getNextPosition(tog, position)
-                                    when(nextPosition) {
-                                        "left" -> {
-                                            turnoutTable.messageToJmri(tog.leftTurnout, "state", TurnoutState.THROWN)
-                                            turnoutTable.messageToJmri(tog.rightTurnout, "state", TurnoutState.CLOSED)
-                                        }
-                                        "mid" -> {
-                                            turnoutTable.messageToJmri(tog.leftTurnout, "state", TurnoutState.CLOSED)
-                                            turnoutTable.messageToJmri(tog.rightTurnout, "state", TurnoutState.CLOSED)
-                                        }
-                                        "right" -> {
-                                            turnoutTable.messageToJmri(tog.leftTurnout, "state", TurnoutState.CLOSED)
-                                            turnoutTable.messageToJmri(tog.rightTurnout, "state", TurnoutState.THROWN)
-                                        }
-                                    }
-                                }
-                            }
-                            is TurnoutGroup.DoubleSlipTurnout -> {
-                                if (newValue == true) {
-                                    val position = turnoutGroupPosition(tog)
-                                    val nextPosition = getNextPosition(tog, position)
-                                    when(nextPosition) {
-                                        "straight" -> {
-                                            turnoutTable.messageToJmri(tog.westTurnout, "state", TurnoutState.CLOSED)
-                                            turnoutTable.messageToJmri(tog.eastTurnout, "state", TurnoutState.CLOSED)
-                                        }
-                                        "cross" -> {
-                                            turnoutTable.messageToJmri(tog.westTurnout, "state", TurnoutState.THROWN)
-                                            turnoutTable.messageToJmri(tog.eastTurnout, "state", TurnoutState.THROWN)
-                                        }
-                                        "turnWest" -> {
-                                            turnoutTable.messageToJmri(tog.westTurnout, "state", TurnoutState.THROWN)
-                                            turnoutTable.messageToJmri(tog.eastTurnout, "state", TurnoutState.CLOSED)
-                                        }
-                                        "turnEast" -> {
-                                            turnoutTable.messageToJmri(tog.westTurnout, "state", TurnoutState.CLOSED)
-                                            turnoutTable.messageToJmri(tog.eastTurnout, "state", TurnoutState.THROWN)
-                                        }
-                                    }
-                                }
-                            }
+                    if (tog != null && newValue == true) {
+                        val position = turnoutGroupPosition(tog)
+                        val nextPosition = getNextPosition(tog, position)
+                        setTurnoutStates(tog, nextPosition) { turnout, state ->
+                            turnoutTable.messageToJmri(turnout, "state", state)
                         }
                     }
                     return
